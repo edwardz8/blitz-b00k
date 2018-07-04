04f9de48af7efc780d64d2082699ccc8d924088f
Zach Edwards <tolklorist@gmail.com>
2018-07-04 14:08:46 -0500

    added auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { MaterialModule } from './shared/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './auth.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB0PTetTnX46UAz_7k2_8mIS6neAYQzwW0',
+    authDomain: 'blitz-book.firebaseapp.com',
+    databaseURL: 'https://blitz-book.firebaseio.com',
+    projectId: 'blitz-book',
+    storageBucket: 'blitz-book.appspot.com',
+    messagingSenderId: '796442872108'
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
